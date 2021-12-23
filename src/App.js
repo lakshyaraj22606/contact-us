b1c8dbb831aa7c735f3c72a13b8c238d2618a1df
lakshyaraj22606 <77723521+lakshyaraj22606@users.noreply.github.com>
2021-12-24 02:00:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Contact from './pages/contact';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <Contact />;
+}
+
+export default App;
---
+++ src/pages/contact.js
@@ -0,0 +1,255 @@
+import React from 'react';
+class Contact extends React.Component{
+    render(){
+        return(
+        <div>
+
+<div class="background">
+  <div class="container">
+    <div class="screen">
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <div class="app-title">
+            <span>CONTACT</span>
+            <span>US</span>
+          </div>
+          <div class="app-contact">CONTACT INFO : +62 81 314 928 595</div>
+        </div>
+        <div class="screen-body-item">
+          <div class="app-form">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME"/>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL"/>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="CONTACT NO"/>
+            </div>
+            <div class="app-form-group message">
+              <input class="app-form-control" placeholder="MESSAGE"/>
+            </div>
+            <div class="app-form-group buttons">
+              <button class="app-form-button">SEND</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="headerwork">
+    ORGANIZERS
+  </div>
+  <div class="team-section">
+  <section>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">Sanjay Kumar</div>
+        <div class="email">2025UCP1234@mnit.ac.in</div>
+          <div class="mobile">+91 6537653547</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem3.jpg" />
+      <div class="info">
+        <div class="name">Priya Singh</div>
+        <div class="email">2025UMT1344@mnit.ac.in</div>
+          <div class="mobile">+91 5389753474</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem2.jpg" />
+      <div class="info">
+        <div class="name">Rahul Singh</div>
+        <div class="email">2025UCE5534@mnit.ac.in</div>
+          <div class="mobile">+91 7075803558</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+  </section>
+
+</div>
+
+<div class="headerwork">
+    MARKETING
+  </div>
+  <div class="team-section">
+  <section>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">Sanjay Kumar</div>
+        <div class="email">2025UCP1234@mnit.ac.in</div>
+          <div class="mobile">+91 6537653547</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem3.jpg" />
+      <div class="info">
+        <div class="name">Priya Singh</div>
+        <div class="email">2025UMT1344@mnit.ac.in</div>
+          <div class="mobile">+91 5389753474</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem2.jpg" />
+      <div class="info">
+        <div class="name">Rahul Singh</div>
+        <div class="email">2025UCE5534@mnit.ac.in</div>
+          <div class="mobile">+91 7075803558</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+  </section>
+
+</div>
+
+<div class="headerwork">
+    MEDIA, PUBLICITY AND ALUMNI RELATIONS
+  </div>
+  <div class="team-section">
+  <section>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">Sanjay Kumar</div>
+        <div class="email">2025UCP1234@mnit.ac.in</div>
+          <div class="mobile">+91 6537653547</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem3.jpg" />
+      <div class="info">
+        <div class="name">Priya Singh</div>
+        <div class="email">2025UMT1344@mnit.ac.in</div>
+          <div class="mobile">+91 5389753474</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem2.jpg" />
+      <div class="info">
+        <div class="name">Rahul Singh</div>
+        <div class="email">2025UCE5534@mnit.ac.in</div>
+          <div class="mobile">+91 7075803558</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+  </section>
+
+</div>
+
+<div class="headerwork">
+    FOOD AND BEVERAGES
+  </div>
+  <div class="team-section">
+  <section>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">Sanjay Kumar</div>
+        <div class="email">2025UCP1234@mnit.ac.in</div>
+          <div class="mobile">+91 6537653547</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem3.jpg" />
+      <div class="info">
+        <div class="name">Priya Singh</div>
+        <div class="email">2025UMT1344@mnit.ac.in</div>
+          <div class="mobile">+91 5389753474</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem2.jpg" />
+      <div class="info">
+        <div class="name">Rahul Singh</div>
+        <div class="email">2025UCE5534@mnit.ac.in</div>
+          <div class="mobile">+91 7075803558</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+  </section>
+
+</div>
+
+        </div>
+        );
+    }
+}
+
+export default Contact;
